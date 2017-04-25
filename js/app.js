566cc956d5104c0389724d0c9eb61d48801d2eff
Administrator <Administrator@WENJU-PC>
2017-04-25 14:48:09 +0800

    裂变红包

--- js/app.js
+++ js/app.js
@@ -239,13 +239,6 @@ var UserObj={
 	setScore : function(score) {
 		setLocalStorage("USER_SCORE",score);
 	},
-	/*获取用户余额*/
-	getMoney : function() {
-		return localStorage.getItem("USER_Money")||"0.00";
-	},
-	setMoney : function(money) {
-		setLocalStorage("USER_Money",money);
-	},
 	/*获取用户登录的token*/
 	getTK : function() {
 		return localStorage.getItem("TK");
